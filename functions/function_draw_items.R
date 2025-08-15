472ec7573dd561933f4fd226aeb806f7a94a5800
Sabrina Ecker <sabrina.ecker@t-online.de>
2025-08-15 14:20:00 +0200

    updated function and cleaned script

--- functions/function_draw_items.R
+++ functions/function_draw_items.R
@@ -9,66 +9,64 @@
 ###################################################################
 
 
-load("prepared data/benchmark_data_Study1.rda")
-
-all_items <- c('aerger1', 'aerger2', 'aerger3',
-               'traurigkeit1', 'traurigkeit2', 'traurigkeit3',
-               'angst1', 'angst2', 'angst3',
-               'scham1', 'scham2', 'scham3',
-               'schuld1', 'schuld2', 'schuld3')
-
-
-sample(all_items, 5, replace=FALSE)
-
-n_items <- 5
-sample(all_items, n_items, replace=FALSE)
-
-
-n_items <- c(5,10)
-# sample(all_items, n_items, replace=FALSE)
-apply(matrix(n_items), MARGIN = 1, FUN = function(n_item) {
-  sample(all_items, n_item, replace=FALSE)
-})
+# Note: In order to subset data frames by variable (item) names as
+# stored in the output of the function, the items need to be extracted
+# from the output as vector:
+# item_vec <- strsplit(output[row,"items"], ", ")[[1]] # extract items as vector
+# item_vec
 
 
 
-draws <- t(apply(matrix(n_items), MARGIN = 1, FUN = function(n_item) {
-  as.vector(sample(all_items, n_item, replace=FALSE))
-}))
-draws
-draws[1,1]
+# Function to Draw Items --------------------------------------------------
 
-#### 
-n_items <- c(5, 10)
-
-items_drawn <- matrix(data=NA, nrow=length(n_items), ncol=2)
-colnames(items_drawn) <- c("n_items", "items")
-
-items_drawn[] <- t(apply(matrix(n_items), 1, FUN = function(nr.items) {
-  drawn <- sample(all_items, nr.items, replace = FALSE)
-  cbind(nr.items, paste(sprintf("'%s'", drawn), collapse = ", "))
-}))
-
-####
-
-as.data.frame(t(apply(matrix(n_items), 1, FUN = function(nr.items) {
-  drawn <- sample(all_items, nr.items, replace = FALSE)
-  cbind(nr.items, paste(sprintf("'%s'", drawn), collapse = ", "))
-})))
-
-######
-
-n_items <- c(5, 10)
-
-items_drawn <- as.data.frame(matrix(data=NA, nrow=length(n_items), ncol=2))
-colnames(items_drawn) <- c("n_items", "items")
-
-items_drawn[] <- t(apply(matrix(n_items), 1, FUN = function(nr.items) {
-  drawn <- sample(all_items, nr.items, replace = FALSE)
-  cbind(nr.items, paste(sprintf("'%s'", drawn), collapse = ", "))
-}))
-
-items_drawn[ , "n_items"] <- as.numeric(items_drawn[ , "n_items"])
-items_drawn[ , "items"] <- as.character(items_drawn[ , "items"])
+draw_items <- function(all_items, n_items) {
+  # all_items: chr vector indicating the variable names of all items as used in the data frame
+  # n_items: number of items to draw
+  
+  # create storage
+  items_drawn <- as.data.frame(matrix(data=NA, nrow=length(n_items), ncol = 2))
+  colnames(items_drawn) <- c("n_items", "items")
+  
+  # draw items and store them in the df
+  items_drawn[] <- t(apply(matrix(n_items), 1, # apply the function to all numbers of items (and transpose output)
+                           FUN = function(nr.items) { # function is a function of every single item number
+                             # draw number of items (nr.items) from all items (all_items) without replacement
+                             drawn <- sample(all_items, nr.items, replace = FALSE)
+                             # bind the number of items and the drawn items
+                             cbind(nr.items, paste(drawn, collapse = ", "))
+                           }))
+  return(items_drawn)
+}
 
 
+
+# # Test function
+# df <- draw_items(all_items = c("item1", "item2", "item3", "item4"), n_items = c(1,2,3))
+# df
+# 
+# set.seed(123)
+# draw_items(all_items = c("item1", "item2", "item3", "item4"), n_items = c(1,2,3))
+# df[3, 2]
+# 
+# rm(df)
+#
+#
+# # Test whether data frame can be subsetted based on the items drawn
+# load("prepared data/benchmark_data_Study1.rda")
+# 
+# output <- draw_items(all_items = c('aerger1', 'aerger2', 'aerger3',
+#                                   'traurigkeit1', 'traurigkeit2', 'traurigkeit3',
+#                                   'angst1', 'angst2', 'angst3',
+#                                   'scham1', 'scham2', 'scham3',
+#                                   'schuld1', 'schuld2', 'schuld3'),
+#                     n_items = c(3, 5, 10))
+# 
+# output[1, 2]
+# 
+# item_vec <- strsplit(output[1,2], ", ")[[1]] # extract items as vector
+# item_vec
+# 
+# sub <- bench[ , c("SERIAL", item_vec)] # first row of items: 3 items -> sub should have 4 columns (with SERIAL)
+# # works
+# 
+# rm(list=ls())
