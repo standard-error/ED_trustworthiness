42435bb6bb9a216ec58ced5b266533e2cfe63f22
Sabrina Ecker <sabrina.ecker@t-online.de>
2025-08-21 17:53:12 +0200

    Updated the plotting function to handle group-specific input data

--- functions/function_plot_outcomes.R
+++ functions/function_plot_outcomes.R
@@ -26,12 +26,15 @@ my_theme <- theme_bw() +
 
 
 # Function for Data Visualization (Overall) -------------------------------
-plot_outcome <- function(data, ylims=NULL, ylabel=NULL, x_breaks, theme_custom = my_theme) {
+plot_outcome <- function(data, ylims=NULL, ylabel=NULL, x_breaks = seq(0, 100, 10), theme_custom = my_theme,
+                         groupwise = FALSE) {
   # data : data frame with the results
   # ylims: optional y-limit per outcome 
   # ylabel: optional y-axis label
   # x_breaks: breaks to use on x-axis, e.g., seq(0, 100, 10)
   # theme_custom : ggplot theme
+  # groupwise: logical indicating whether or not the data contains high/medium/low NED group 
+          # and if it should be plotted groupwise
   
 
   # Identify mean, min, max columns automatically from data frame
@@ -48,23 +51,41 @@ plot_outcome <- function(data, ylims=NULL, ylabel=NULL, x_breaks, theme_custom =
   # drawn as split by facet_wrap
   # -> however, do not use facet_wrap but ggh4x::facet_nested, so that the 
   # title spans across both plots
-  data[ , "facet_group"] <- "Occasions Drawn"
+  data[ , "facet_occ"] <- "Occasions Drawn"
+  # also do it for group (will not be used if groupwise  == FALSE)
+  data[ , "facet_group"] <- "NED Level"
   
   
-  # plot the data
-  p <- ggplot(data, aes(x = n_occasions, y = .data[[col_mean]])) +
-    geom_point(aes(shape = factor(n_items), color = factor(n_items)),
-               position = position_dodge(width = 2)) +
-    geom_line(aes(color = factor(n_items)), linewidth = 0.3) +
-    geom_errorbar(aes(ymin = .data[[col_min]], ymax = .data[[col_max]],
-                      color = factor(n_items)),
-                  position = position_dodge(width = 2)) +
+  # Check if groupwise = TRUE
+  # adjust facets accordingly
+  # for groupwise = FALSE: facet only by occasions drawn
+  # for groupwise = TRUE: also facet by NED group
+  if (groupwise == TRUE & !("group" %in% names(data))) {
+    stop(sprintf("There is no grouping variable for NED group in data."))
+  } else if (groupwise == TRUE & "group" %in% names(data)) {
+    facet_formula <- ggh4x::facet_nested(~facet_group + factor(group) + facet_occ + factor(occasions_drawn))
+  } else if (groupwise == FALSE ) {
+    facet_formula <- ggh4x::facet_nested(~facet_occ + factor(occasions_drawn))
+  }
+  
+  # Build plot
+  p <- ggplot(data, aes(
+      x = n_occasions, # x axis: n_occasions
+      y = .data[[col_mean]], # y axis: mean outcome
+      color = factor(n_items), # different lines for n_items
+      shape = factor(n_items),
+      group = factor(n_items)
+    )) +
+    geom_point(position = position_dodge(width = 2)) +
+    geom_line(linewidth = 0.3) +
+    geom_errorbar(aes(ymin = .data[[col_min]], ymax = .data[[col_max]]),
+                  position = position_dodge(width = 2)) + # error bar: min and max outcome
     scale_x_continuous(breaks = x_breaks) +
-    labs(shape = "Number of Items", color = "Number of Items") +
     xlab("Number of Occasions") +
     #  if y label is provided, use it; else, use the outcome name extracted from column names of data 
     ylab(ifelse(!is.null(ylabel), ylabel, outcome_name)) +
-    ggh4x::facet_nested(~facet_group + factor(occasions_drawn)) +
+    labs(color = "Number of Items", shape = "Number of Items") +
+    facet_formula +
     scale_color_viridis_d(option = "magma", begin = 0.20, end = 0.80) +
     theme_custom
   
@@ -73,7 +94,7 @@ plot_outcome <- function(data, ylims=NULL, ylabel=NULL, x_breaks, theme_custom =
   }
   
   return(p)
-
+  
 }
 
 
@@ -83,8 +104,14 @@ plot_outcome <- function(data, ylims=NULL, ylabel=NULL, x_breaks, theme_custom =
 # # extract examplary data
 # dat <- agg[["cor_ICC"]][["agg_res"]]
 # 
-# plot_outcome(data=dat, ylims = c(-0.20, 1.0), ylabel = NULL, theme_custom = my_theme)
+# plot_outcome(data=dat, ylims = c(-0.20, 1.0), ylabel = NULL, x_breaks = seq(0,100,10), theme_custom = my_theme,
+#              groupwise = FALSE)
 # rm(dat, agg)
+# 
+# load("results/aggregated_subgroups_Study1.rda")
+# dat <- agg_grp[["cor_ICC"]][["agg_res"]]
+# plot_outcome(data=dat, ylims=c(-0.20, 1.0), ylabel=NULL, x_breaks = seq(0,100,10), theme_custom=my_theme,
+#              groupwise = TRUE)
 
 
 # # Notes (plot without function)
@@ -103,9 +130,84 @@ plot_outcome <- function(data, ylims=NULL, ylabel=NULL, x_breaks, theme_custom =
 #   facet_wrap(~factor(occasions_drawn)) +
 #   scale_color_viridis_d(option="magma", begin = 0.20, end=0.80) +
 #   my_theme
-# 
+#
 # test
 
 
 
-
+# # Note: Group as additional grouping variable, but not faceted
+# plot_outcome <- function(data, ylims=NULL, ylabel=NULL, x_breaks = seq(0, 100, 10), theme_custom = my_theme,
+#                          groupwise = FALSE) {
+#   # data : data frame with the results
+#   # ylims: optional y-limit per outcome 
+#   # ylabel: optional y-axis label
+#   # x_breaks: breaks to use on x-axis, e.g., seq(0, 100, 10)
+#   # theme_custom : ggplot theme
+#   # groupwise: logical indicating whether or not the data contains high/medium/low NED group 
+#   # and if it should be plotted groupwise
+#   
+#   
+#   # Identify mean, min, max columns automatically from data frame
+#   col_mean <- grep("_mean$", names(data), value = TRUE)
+#   col_min  <- grep("_min$", names(data), value = TRUE)
+#   col_max  <- grep("_max$", names(data), value = TRUE)
+#   
+#   # read the outcome name from the last column in data (should be outcome_max)
+#   # last column = length(data)
+#   outcome_name <- sub("_max$", "", names(data)[length(data)])
+#   
+#   
+#   # Workaround so that we have a title above "random" and "by order" for occasions
+#   # drawn as split by facet_wrap
+#   # -> however, do not use facet_wrap but ggh4x::facet_nested, so that the 
+#   # title spans across both plots
+#   data[ , "facet_group"] <- "Occasions Drawn"
+#   
+#   
+#   
+#   # Check if groupwise = TRUE
+#   if (groupwise == TRUE & "group" %in% names(data)) {
+#     aes_settings <- aes(
+#       x = n_occasions,
+#       y = .data[[col_mean]],
+#       color = factor(group), #  use both group and n_items as grouping variables
+#       shape = factor(n_items),
+#       group = interaction(group, n_items) # "interaction" between groups and items
+#       # -> each group with each number of items
+#     )
+#     legend.labels <- labs(color = "NED Group", shape = "Number of Items")
+#   } else if (groupwise == TRUE & !("group" %in% names(data))) {
+#     stop(sprintf("There is no grouping variable for NED group in data."))
+#   } else if (groupwise == FALSE ) {
+#     aes_settings <- aes(
+#       x = n_occasions,
+#       y = .data[[col_mean]],
+#       color = factor(n_items),
+#       shape = factor(n_items),
+#       group = factor(n_items)
+#     ) 
+#     legend.labels <- labs(color = "Number of Items", shape = "Number of Items")
+#   }
+#   
+#   # Build plot
+#   p <- ggplot(data, aes_settings) +
+#     geom_point(position = position_dodge(width = 2)) +
+#     geom_line(linewidth = 0.3) +
+#     geom_errorbar(aes(ymin = .data[[col_min]], ymax = .data[[col_max]]),
+#                   position = position_dodge(width = 2)) +
+#     scale_x_continuous(breaks = x_breaks) +
+#     xlab("Number of Occasions") +
+#     #  if y label is provided, use it; else, use the outcome name extracted from column names of data 
+#     ylab(ifelse(!is.null(ylabel), ylabel, outcome_name)) +
+#     legend.labels +
+#     ggh4x::facet_nested(~facet_group + factor(occasions_drawn)) +
+#     scale_color_viridis_d(option = "magma", begin = 0.20, end = 0.80) +
+#     theme_custom
+#   
+#   if (!is.null(ylims)) {
+#     p <- p + scale_y_continuous(limits = ylims, breaks = scales::breaks_pretty(n = 5))
+#   }
+#   
+#   return(p)
+#   
+# }
