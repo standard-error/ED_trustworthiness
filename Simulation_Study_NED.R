fffadb6d67828b77b1f9d0640e9e008b943839be
Sabrina Ecker <sabrina.ecker@t-online.de>
2026-04-22 18:02:38 +0200

    updated simulation study (item sets balanced across replications), renamed to indicate that target is NED

--- Simulation_Study_NED.R
+++ Simulation_Study_NED.R
@@ -1,11 +1,16 @@
 ###################################################################
-#####    Estimating trait negative emotion differentiation:   #####
-#####        How many measurement occasions and               #####
-#####              emotion items are needed?                  #####
+#####      Estimating trait emotion differentiation:          #####
+#####          How many measurement occasions and             #####
+#####               emotion items are needed?                 #####
 ###################################################################
 
 ###################################################################
 #####                   Simulation Study                      #####
+###################################################################
+
+
+###################################################################
+#####             Negative Emotion Differentiation            #####
 ###################################################################
 
 
@@ -19,11 +24,6 @@ is.numeric(bench$SERIAL)
 
 
 # Source Functions --------------------------------------------------------
-# source("functions/function_ordered_occasion_draw.R")
-# source("functions/function_random_occasion_draw.R")
-# source("functions/function_calculate_iccs.R")
-# source("functions/function_one_simulation_run.R")
-
 source("functions/function_simulation_study.R")
 # simulation study function already sources the other functions needed
 # also already loads packages that are needed (future, future.apply)
@@ -33,12 +33,14 @@ source("functions/function_simulation_study.R")
 
 # Global Seed Item Set ----------------------------------------------------
 
-##### IMPORTANT: USE THE SAME SEED SO THAT THE SAME ITEM SETS
+##### IMPORTANT: IF ITEM SETS SHALL BE FIXED ACROSS REPLICATIONS
+##### USE THE SAME SEED SO THAT THE SAME ITEM SETS
 ##### ARE DRAWN FOR OVERALL SIMULATION IF RUN TWICE 
 
+# source("Global_Seed_Item_Sets.R")
 
-source("Global_Seed_Item_Sets.R")
-
+# here: not necessary, because item sets shall be drawn in balanced manner
+# across replications
 
 
 
@@ -59,14 +61,15 @@ res <- simulation_study(data = bench, n_occasions = c(14, seq(20, 70, 10)),
                                        "scham", "scham", "scham",
                                        "schuld", "schuld", "schuld"),
                         type = "consistency", unit = "single", occ.running.var = "occ_running",
-                        seed_item = global.seed.item.set, seed_sim = 123, cores = 11)
+                        item_sets_across_replications = "balanced",
+                        seed_item = 140821, seed_sim = 123, cores = 11)
 tictoc::toc()
 
 
 
 
 # Save Results ------------------------------------------------------------
-save(res, file="results/sim_results.rda")
+save(res, file="results/02_revision1/sim_results_NED.rda")
 
 
 
@@ -74,7 +77,6 @@ save(res, file="results/sim_results.rda")
 # i.e., run simulation again with 5000 iterations and
 # different seed
 # -> check whether results are comparable
-# (but use the same seed for the item set)
 tictoc::tic()
 res2 <- simulation_study(data = bench, n_occasions = c(14, seq(20, 70, 10)),
                          occasions_drawn = c("random", "by order"), n_items = c(5, 10, 15),
@@ -90,11 +92,12 @@ res2 <- simulation_study(data = bench, n_occasions = c(14, seq(20, 70, 10)),
                                         "scham", "scham", "scham",
                                         "schuld", "schuld", "schuld"),
                          type = "consistency", unit = "single", occ.running.var = "occ_running",
-                         seed_item = global.seed.item.set, seed_sim = 456, cores = 11)
+                         
+                         seed_item = 210807, seed_sim = 456, cores = 11)
 tictoc::toc()
 
 # save results
-save(res2, file="results/check nr of iterations/sim_results.rda")
+save(res2, file="results/02_revision_1/check nr of iterations/sim_results_NED.rda")
 
 
 
